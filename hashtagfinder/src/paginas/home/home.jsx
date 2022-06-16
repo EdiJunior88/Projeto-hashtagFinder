238ec1ba990e5a4f4b01457c0574b539b7f9b0bb
Edivaldo Junior <er.moura88@yahoo.com.br>
2022-06-16 13:25:47 -0300

    Atualização no Projeto - Atualização da página home - Inserção das imagens e ícones - Distribuição das imagens por pastas

--- hashtagfinder/src/paginas/home/home.jsx
+++ hashtagfinder/src/paginas/home/home.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import Reset from '../../css/reset.css';
+import Cabecalho_Imagem from '../../imagens/cabecalho/cabecalho.jpg';
 
 const Home = () => {
   return (
     <>
-      <p>Teste da Página Home</p>
+      <header>
+        <div className='container-cabecalho'>
+          <nav className='cabecalho'>
+            <div className='cabecalho-titulo'>
+              <span>hashtag</span>
+              <span className='texto-negrito'>finder</span>
+            </div>
+
+            <div className='cabecalho-botoes'>
+              <button typeof='button'>Sobre</button>
+              <button typeof='button'>Login</button>
+            </div>
+          </nav>
+
+          <div className='cabecalho-imagem'>
+            <img src={Cabecalho_Imagem} />
+          </div>
+        </div>
+      </header>
     </>
   );
 };
