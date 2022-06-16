5e9f2f5ca1739e412754770d119f84d58c10ffa9
Edivaldo Junior <er.moura88@yahoo.com.br>
2022-06-16 16:22:29 -0300

    Reajustando o App - Melhor divisão do componente cabeçalho e da página HOME - Ajustes no CSS Global - Criação do CSS cabeçalho junto com o componente cabeçalho - Ajustes no CSS HOME

---
+++ hashtagfinder/src/componentes/cabecalho/cabecalho.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import '../../css/cabecalho.css'
+import sobreIcone from '../../imagens/icones/icon-info-circle.svg';
+import loginIcone from '../../imagens/icones/icon-user-alt.svg';
+
+export default function Cabecalho() {
+  return (
+    <>
+      <header>
+        <div className='container-cabecalho'>
+          <nav className='cabecalho'>
+            <div className='cabecalho-titulo'>
+              <span>hashtag</span>
+              <span className='texto-negrito'>finder</span>
+            </div>
+
+            <div className='cabecalho-botoes'>
+              <button className='botao-sobre'>
+                <img  className='botao-icone' src={sobreIcone} />
+                Sobre
+              </button>
+
+              <button className='botao-login'>
+                <img className='botao-icone' src={loginIcone} />
+                Login
+              </button>
+            </div>
+          </nav>
+        </div>
+      </header>
+    </>
+  );
+}
--- hashtagfinder/src/paginas/home/home.jsx
+++ hashtagfinder/src/paginas/home/home.jsx
@@ -1,33 +1,26 @@
 import React from 'react';
-import '../../css/reset.css';
 import '../../css/global.css';
+import '../../css/home.css';
+import Cabecalho from '../../componentes/cabecalho/cabecalho';
 
 const Home = () => {
   return (
     <>
-      <header>
-        <div className='container-cabecalho'>
-          <nav className='cabecalho'>
-            <div className='cabecalho-titulo'>
-              <span>hashtag</span>
-              <span className='texto-negrito'>finder</span>
-            </div>
+      {
+        <Cabecalho />
+      }
 
-            <div className='cabecalho-botoes'>
-              <button typeof='button'>Sobre</button>
-              <button typeof='button'>Login</button>
-            </div>
-          </nav>
-
-          <div className='cabecalho-descricao'>
-            <h1>Encontre hashtags de maneira fácil</h1>
-            <h2>
-              Digite o que deseja no campo de buscas e confira os resultados do
-              Twitter abaixo
-            </h2>
-          </div>
+      {
+        <div className='cabecalho-descricao'>
+          <h1 className='descricao-titulo'>
+            Encontre hashtags de maneira fácil
+          </h1>
+          <h2 className='descricao-subtitulo'>
+            Digite o que deseja no campo de buscas e confira os resultados do
+            Twitter abaixo
+          </h2>
         </div>
-      </header>
+      }
     </>
   );
 };
