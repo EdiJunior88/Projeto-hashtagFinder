25eee193be8f05fa202e97fcbd3de18daa3932aa
Edivaldo Junior <er.moura88@yahoo.com.br>
2022-06-18 19:47:12 -0300

    Atualizações no Projeto - Inclusão da Biblioteca Swiper (Galeria de Imagens) - Criação do componente Galeria - Criação do CSS do componente Galeria - Estilização da Galeria para testes - Responsividade da Galeria apenas na tela 1920px

---
+++ hashtagfinder/src/componentes/galeria/galeria.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+
+/* Importação das bibliotecas padrões do SwiperJS */
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import 'swiper/css/bundle';
+
+import './galeria.css';
+
+export default function Galeria() {
+  return (
+    <section className='container galeria'>
+      <div className='container-descricao-galeria'>
+        <p className='descricao-galeria-texto'>Exibindo os 10 resultados mais recentes para #natureza</p>
+      </div>
+
+      <div className='container-galeria'>
+        <Swiper
+          slidesPerView={5}
+          slidesPerGroup={3}
+          loop={false}
+          loopFillGroupWithBlank={true}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={true}
+          modules={[Pagination, Navigation]}
+          className='mySwiper'>
+          <SwiperSlide>
+            <img src='https://swiperjs.com/demos/images/nature-1.jpg' alt='' />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src='https://swiperjs.com/demos/images/nature-2.jpg' alt='' />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src='https://swiperjs.com/demos/images/nature-3.jpg' alt='' />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src='https://swiperjs.com/demos/images/nature-4.jpg' alt='' />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src='https://swiperjs.com/demos/images/nature-5.jpg' alt='' />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src='https://swiperjs.com/demos/images/nature-6.jpg' alt='' />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src='https://swiperjs.com/demos/images/nature-7.jpg' alt='' />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src='https://swiperjs.com/demos/images/nature-8.jpg' alt='' />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src='https://swiperjs.com/demos/images/nature-10.jpg' alt='' />
+          </SwiperSlide>
+        </Swiper>
+      </div>
+    </section>
+  );
+}
--- hashtagfinder/src/paginas/home/home.jsx
+++ hashtagfinder/src/paginas/home/home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Busca from '../../componentes/busca/busca';
 import Cabecalho from '../../componentes/cabecalho/cabecalho';
 import Descricao from '../../componentes/descricao/descricao';
+import Galeria from '../../componentes/galeria/galeria';
 import Rodape from '../../componentes/rodape/rodape';
 import '../../css/global.css'
 import './home.css'
@@ -12,6 +13,7 @@ function Home() {
       <Cabecalho />
       <Descricao />
       <Busca />
+      <Galeria />
       <Rodape />
     </div>
   );
