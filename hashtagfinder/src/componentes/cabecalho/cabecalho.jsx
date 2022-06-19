05b11d9e2c5566f82d6378f4193ff42cf51dcdcd
Washington <wluizs245@gmail.com>
2022-06-19 16:41:41 -0300

    configurado link do repositorio github dentro do package.json / importação do cabecalho pro componente SOBRE/ criado CSS da pagina SOBRE

--- hashtagfinder/src/componentes/cabecalho/cabecalho.jsx
+++ hashtagfinder/src/componentes/cabecalho/cabecalho.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../../css/cabecalho.css'
+import '../../css/cabecalho.css';
 import sobreIcone from '../../imagens/icones/icon-info-circle.svg';
 import loginIcone from '../../imagens/icones/icon-user-alt.svg';
 
@@ -7,21 +7,21 @@ export default function Cabecalho() {
   return (
     <>
       <header>
-        <div className='container-cabecalho'>
-          <nav className='cabecalho'>
-            <div className='cabecalho-titulo'>
+        <div className="container-cabecalho">
+          <nav className="cabecalho">
+            <div className="cabecalho-titulo">
               <span>hashtag</span>
-              <span className='texto-negrito'>finder</span>
+              <span className="texto-negrito">finder</span>
             </div>
 
-            <div className='cabecalho-botoes'>
-              <button className='botao-sobre'>
-                <img  className='botao-icone' src={sobreIcone} />
+            <div className="cabecalho-botoes">
+              <button className="botao-sobre">
+                <img className="botao-icone" src={sobreIcone} />
                 Sobre
               </button>
 
-              <button className='botao-login'>
-                <img className='botao-icone' src={loginIcone} />
+              <button className="botao-login">
+                <img className="botao-icone" src={loginIcone} />
                 Login
               </button>
             </div>
---
+++ hashtagfinder/src/paginas/sobre/sobre.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Cabecalho from '../../componentes/cabecalho/cabecalho';
+
+export const sobre = () => {
+  return (
+    <>
+      <Cabecalho />
+      <div>Sobre o projeto</div>
+      <div>O que é</div>
+    </>
+  );
+};
