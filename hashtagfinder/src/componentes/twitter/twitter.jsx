42b8f30921d4b23b86c3e23b6918682abc51f936
Edivaldo Junior <er.moura88@yahoo.com.br>
2022-06-20 17:54:56 -0300

    Componente Twitter - Finalização da responsividade das telas (1920px / 1305px / 100px / 770px / 414px)
    - Componente Rodapé
    - Finalização da responsividade da tela 414px

--- hashtagfinder/src/componentes/twitter/twitter.jsx
+++ hashtagfinder/src/componentes/twitter/twitter.jsx
@@ -50,6 +50,236 @@ export default function Twitter() {
           </div>
         </div>
       </div>
+
+      <div className='container-twitter'>
+        <div className='twitter'>
+          <div className='container-twitter-foto'>
+            <img className='twitter-foto' src={FotoPerfil} alt='foto perfil' />
+          </div>
+
+          <div className='container-twitter-descricao'>
+            <div className='container-twitter-nome-usuario'>
+              <span className='twitter-nome'>UserName</span>
+              <span className='twitter-usuario'>@twitterusername</span>
+            </div>
+            <p className='twitter-descricao'>
+              Lorem ipsum dolor sit amet. Ut odit necessitatibus quo dolorem
+              placeat est provident accusantium qui iure explicabo. Sit
+              consequatur provident et quod
+            </p>
+            <a className='twitter-link' href='#'>
+              Ver mais no Twitter
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div className='container-twitter'>
+        <div className='twitter'>
+          <div className='container-twitter-foto'>
+            <img className='twitter-foto' src={FotoPerfil} alt='foto perfil' />
+          </div>
+
+          <div className='container-twitter-descricao'>
+            <div className='container-twitter-nome-usuario'>
+              <span className='twitter-nome'>UserName</span>
+              <span className='twitter-usuario'>@twitterusername</span>
+            </div>
+            <p className='twitter-descricao'>
+              Lorem ipsum dolor sit amet. Ut odit necessitatibus quo dolorem
+              placeat est provident accusantium qui iure explicabo. Sit
+              consequatur provident et quod
+            </p>
+            <a className='twitter-link' href='#'>
+              Ver mais no Twitter
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div className='container-twitter'>
+        <div className='twitter'>
+          <div className='container-twitter-foto'>
+            <img className='twitter-foto' src={FotoPerfil} alt='foto perfil' />
+          </div>
+
+          <div className='container-twitter-descricao'>
+            <div className='container-twitter-nome-usuario'>
+              <span className='twitter-nome'>UserName</span>
+              <span className='twitter-usuario'>@twitterusername</span>
+            </div>
+            <p className='twitter-descricao'>
+              Lorem ipsum dolor sit amet. Ut odit necessitatibus quo dolorem
+              placeat est provident accusantium qui iure explicabo. Sit
+              consequatur provident et quod
+            </p>
+            <a className='twitter-link' href='#'>
+              Ver mais no Twitter
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div className='container-twitter'>
+        <div className='twitter'>
+          <div className='container-twitter-foto'>
+            <img className='twitter-foto' src={FotoPerfil} alt='foto perfil' />
+          </div>
+
+          <div className='container-twitter-descricao'>
+            <div className='container-twitter-nome-usuario'>
+              <span className='twitter-nome'>UserName</span>
+              <span className='twitter-usuario'>@twitterusername</span>
+            </div>
+            <p className='twitter-descricao'>
+              Lorem ipsum dolor sit amet. Ut odit necessitatibus quo dolorem
+              placeat est provident accusantium qui iure explicabo. Sit
+              consequatur provident et quod
+            </p>
+            <a className='twitter-link' href='#'>
+              Ver mais no Twitter
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div className='container-twitter'>
+        <div className='twitter'>
+          <div className='container-twitter-foto'>
+            <img className='twitter-foto' src={FotoPerfil} alt='foto perfil' />
+          </div>
+
+          <div className='container-twitter-descricao'>
+            <div className='container-twitter-nome-usuario'>
+              <span className='twitter-nome'>UserName</span>
+              <span className='twitter-usuario'>@twitterusername</span>
+            </div>
+            <p className='twitter-descricao'>
+              Lorem ipsum dolor sit amet. Ut odit necessitatibus quo dolorem
+              placeat est provident accusantium qui iure explicabo. Sit
+              consequatur provident et quod
+            </p>
+            <a className='twitter-link' href='#'>
+              Ver mais no Twitter
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div className='container-twitter'>
+        <div className='twitter'>
+          <div className='container-twitter-foto'>
+            <img className='twitter-foto' src={FotoPerfil} alt='foto perfil' />
+          </div>
+
+          <div className='container-twitter-descricao'>
+            <div className='container-twitter-nome-usuario'>
+              <span className='twitter-nome'>UserName</span>
+              <span className='twitter-usuario'>@twitterusername</span>
+            </div>
+            <p className='twitter-descricao'>
+              Lorem ipsum dolor sit amet. Ut odit necessitatibus quo dolorem
+              placeat est provident accusantium qui iure explicabo. Sit
+              consequatur provident et quod
+            </p>
+            <a className='twitter-link' href='#'>
+              Ver mais no Twitter
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div className='container-twitter'>
+        <div className='twitter'>
+          <div className='container-twitter-foto'>
+            <img className='twitter-foto' src={FotoPerfil} alt='foto perfil' />
+          </div>
+
+          <div className='container-twitter-descricao'>
+            <div className='container-twitter-nome-usuario'>
+              <span className='twitter-nome'>UserName</span>
+              <span className='twitter-usuario'>@twitterusername</span>
+            </div>
+            <p className='twitter-descricao'>
+              Lorem ipsum dolor sit amet. Ut odit necessitatibus quo dolorem
+              placeat est provident accusantium qui iure explicabo. Sit
+              consequatur provident et quod
+            </p>
+            <a className='twitter-link' href='#'>
+              Ver mais no Twitter
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div className='container-twitter'>
+        <div className='twitter'>
+          <div className='container-twitter-foto'>
+            <img className='twitter-foto' src={FotoPerfil} alt='foto perfil' />
+          </div>
+
+          <div className='container-twitter-descricao'>
+            <div className='container-twitter-nome-usuario'>
+              <span className='twitter-nome'>UserName</span>
+              <span className='twitter-usuario'>@twitterusername</span>
+            </div>
+            <p className='twitter-descricao'>
+              Lorem ipsum dolor sit amet. Ut odit necessitatibus quo dolorem
+              placeat est provident accusantium qui iure explicabo. Sit
+              consequatur provident et quod
+            </p>
+            <a className='twitter-link' href='#'>
+              Ver mais no Twitter
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div className='container-twitter'>
+        <div className='twitter'>
+          <div className='container-twitter-foto'>
+            <img className='twitter-foto' src={FotoPerfil} alt='foto perfil' />
+          </div>
+
+          <div className='container-twitter-descricao'>
+            <div className='container-twitter-nome-usuario'>
+              <span className='twitter-nome'>UserName</span>
+              <span className='twitter-usuario'>@twitterusername</span>
+            </div>
+            <p className='twitter-descricao'>
+              Lorem ipsum dolor sit amet. Ut odit necessitatibus quo dolorem
+              placeat est provident accusantium qui iure explicabo. Sit
+              consequatur provident et quod
+            </p>
+            <a className='twitter-link' href='#'>
+              Ver mais no Twitter
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div className='container-twitter'>
+        <div className='twitter'>
+          <div className='container-twitter-foto'>
+            <img className='twitter-foto' src={FotoPerfil} alt='foto perfil' />
+          </div>
+
+          <div className='container-twitter-descricao'>
+            <div className='container-twitter-nome-usuario'>
+              <span className='twitter-nome'>UserName</span>
+              <span className='twitter-usuario'>@twitterusername</span>
+            </div>
+            <p className='twitter-descricao'>
+              Lorem ipsum dolor sit amet. Ut odit necessitatibus quo dolorem
+              placeat est provident accusantium qui iure explicabo. Sit
+              consequatur provident et quod
+            </p>
+            <a className='twitter-link' href='#'>
+              Ver mais no Twitter
+            </a>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
