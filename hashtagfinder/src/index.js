7e40e213d08e520ac7d03e7b66e636eb5d269fa7
sarahrubia <sarahrubia22@gmail.com>
2022-06-20 16:41:06 -0300

    update package.json e index.js

--- hashtagfinder/src/index.js
+++ hashtagfinder/src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-/* import App from './App'; */
-// import Home from './paginas/home/home';
-import Login from './paginas/login/login';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-   {/* <Home /> */}
-   <Login />
-  </React.StrictMode>
+    <App />
+  </React.StrictMode>,
 );
