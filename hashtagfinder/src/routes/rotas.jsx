975d06ad3189364a10d67f8795bba8007ca9ec63
Edivaldo Junior <er.moura88@yahoo.com.br>
2022-06-23 11:39:09 -0300

    Update na pasta rotas

--- hashtagfinder/src/routes/rotas.jsx
+++ hashtagfinder/src/routes/rotas.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import Home from '../paginas/home/home';
 import Login from '../paginas/login/login';
 import Sobre from '../paginas/sobre/sobre';
@@ -7,14 +7,14 @@ import Lista from "../paginas/lista/Lista";
 
 const Rotas = () => {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/sobre" element={<Sobre />} />
         <Route path="/login" element={<Login />} />
         <Route path="/lista" element={<Lista />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 export default Rotas;
