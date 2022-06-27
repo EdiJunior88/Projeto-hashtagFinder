67d57d463b1192a2fb027ecd14e8f522b0caafb1
sarahrubia <sarahrubia22@gmail.com>
2022-06-27 16:57:57 -0300

    Fix: mesclagem de conflito

--- hashtagfinder/src/paginas/login/login.jsx
+++ hashtagfinder/src/paginas/login/login.jsx
@@ -1,18 +1,9 @@
-<<<<<<< HEAD
 import React from "react";
 import styles from "../../componentes/cabecalho/Cabecalho.module.css";
 import styles2 from "./login.module.css";
 import Formulario from "../../componentes/formulario-login/formulario";
 import BotaoHome from "../../componentes/botoes/botaoHome";
 // import { Link } from 'react-router-dom';
-=======
-import React from 'react';
-import styles from '../../componentes/cabecalho/Cabecalho.module.css';
-import styles2 from './login.module.css';
-import Formulario from '../../componentes/formulario-login/formulario';
-import BotaoHome from '../../componentes/botoes/botaoHome';
-import { Link } from 'react-router-dom';
->>>>>>> 025b4a860de1db342dbb553b1ff981086786b615
 
 export default function Login() {
   return (
