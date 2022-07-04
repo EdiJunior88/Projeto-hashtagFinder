a3ad5e4440c1966ddf2988cd62e84ecbafd238f0
Edivaldo Junior <er.moura88@yahoo.com.br>
2022-07-04 11:40:09 -0300

    Atualização da página HOME - Adição do CABECALHO ATIVO ao dar scroll na página
    Atualização do componente CABECALHO
    - Adição de propriedades para ativar o CABECALHO ao dar scroll na página
    - Pequenas atualizações no CSS do CABECALHO (background fosco, cor do texto)

--- hashtagfinder/src/paginas/home/home.jsx
+++ hashtagfinder/src/paginas/home/home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Busca from '../../componentes/busca/busca';
 import Cabecalho from '../../componentes/cabecalho/cabecalho';
 import Descricao from '../../componentes/descricao/descricao';
@@ -8,16 +8,25 @@ import styles from './Home.module.css';
 import '../../css/global.css';
 
 const Home = () => {
+  const [ativaNav, setAtivaNav] = useState(false);
+
+  /* Ativa o cabeçalho após a posição vertical (y) 900 da página */
+  useEffect(() => {
+    function posicaoScroll() {
+      if (window.scrollY >= 100) {
+        setAtivaNav(true);
+      } else {
+        setAtivaNav(false);
+      }
+    }
+    window.addEventListener('scroll', posicaoScroll);
+  }, []);
 
   return (
     <div className={styles.home}>
-      <Cabecalho />
+      <Cabecalho acao={ativaNav} />
       <Descricao />
-      <Busca
-        type='search'
-        placeholder='Buscar...'
-        maxLength={20}
-      />
+      <Busca type='search' placeholder='Buscar...' maxLength={20} />
       <Rodape />
     </div>
   );
