467c81cd5529f9ccebf13d481430a038d2f79072
Edivaldo Junior <er.moura88@yahoo.com.br>
2022-07-10 20:56:40 -0300

    Atualização da página HOME - Adição da função posicaoScroll dentro de um useEffect

--- hashtagfinder/src/paginas/home/home.jsx
+++ hashtagfinder/src/paginas/home/home.jsx
@@ -1,38 +1,36 @@
-import React, { useEffect, useState } from "react";
-import Busca from "../../componentes/busca/busca";
-import Cabecalho from "../../componentes/cabecalho/cabecalho";
-import Descricao from "../../componentes/descricao/descricao";
-import Rodape from "../../componentes/rodape/rodape";
+import React, { useEffect, useState } from 'react';
+import Busca from '../../componentes/busca/busca';
+import Cabecalho from '../../componentes/cabecalho/cabecalho';
+import Descricao from '../../componentes/descricao/descricao';
+import Rodape from '../../componentes/rodape/rodape';
 
-import styles from "./Home.module.css";
-import "../../css/global.css";
+import styles from './Home.module.css';
+import '../../css/global.css';
 
 export default function Home() {
-  const [ativaNav, setAtivaNav] = useState(false);
-
-  /* Altera o título da aba da página */
-  useEffect(() => {
-    document.title = "hashtagfinder | Home";
-  }, [])
+  const [ativaMenu, setAtivaMenu] = useState(false);
 
   /* Ativa o cabeçalho após a posição vertical (y) 900 da página */
-   useEffect(() => {
+  useEffect(() => {
     function posicaoScroll() {
       if (window.scrollY >= 900) {
-        setAtivaNav(true);
+        setAtivaMenu(true);
       } else {
-        setAtivaNav(false);
+        setAtivaMenu(false);
       }
     }
-    window.addEventListener("scroll", posicaoScroll);
+    window.addEventListener('scroll', posicaoScroll);
+
+    /* Altera o título da aba da página */
+    document.title = 'hashtagfinder | Home';
   }, []);
 
   return (
     <div className={styles.home}>
-      <Cabecalho acao={ativaNav} />
+      <Cabecalho acao={ativaMenu} />
       <Descricao />
-      <Busca type="search" placeholder="Buscar..." maxLength={20} />
+      <Busca type='search' placeholder='Buscar...' maxLength={20} />
       <Rodape />
     </div>
   );
-};
+}
