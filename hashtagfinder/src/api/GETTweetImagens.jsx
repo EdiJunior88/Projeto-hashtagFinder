6c404d325990ddc972a8dfbb34cca5c785ee1294
EdiJunior88 <er.moura88@yahoo.com.br>
2022-07-19 18:43:16 -0300

    update

--- hashtagfinder/src/api/GETTweetImagens.jsx
+++ hashtagfinder/src/api/GETTweetImagens.jsx
@@ -1,5 +1,5 @@
-const getTweetImagens = async (searchValue, moreRequest) => {
-  const url = `https://cors.eu.org/https://api.twitter.com/2/tweets/search/recent?query=${searchValue}%20has:hashtags%20-is:retweet%20-is:quote%20has:images&max_results=${moreRequest}&expansions=author_id,attachments.media_keys&user.fields=id,name,username,profile_image_url,url&media.fields=type,url,width,height&tweet.fields=source`;
+const getTweetImagens = async (valorPesquisa, maisRequisicao) => {
+  const url = `https://cors.eu.org/https://api.twitter.com/2/tweets/search/recent?query=${valorPesquisa}%20has:hashtags%20-is:retweet%20-is:quote%20has:images&max_results=${maisRequisicao}&expansions=author_id,attachments.media_keys&user.fields=id,name,username,profile_image_url,url&media.fields=type,url,width,height&tweet.fields=source`;
 
   const token =
     'AAAAAAAAAAAAAAAAAAAAAFlKHgEAAAAApBW4nRyRkiogluzAbXlS4KuHlMU%3DFcR7r8N19LRnMHLVmYlFsod6Be6zUvZD2rxATotl6mLPAh2UEX';
